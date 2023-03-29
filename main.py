020cb3158ce800abea9cb4d935da802c56b8b6d9
rainyday21 <rainydayscholar21@gmail.com>
2023-03-29 14:03:32 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def classical(mat_1, mat_2):
+    rows = mat_1.length()
+    col = mat_2[0].length()
+    mat_f = []
+    for i in (range (0,(mat_1.range()-1))):
+        for j in (range (0, mat_2.range()-1)):
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
