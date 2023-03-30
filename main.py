242f44dead219df5eae4868f71d06bb0e03924d6
rainyday21 <rainydayscholar21@gmail.com>
2023-03-30 16:24:18 -0700

    removed unused text

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 def classical(mat_1, mat_2):
     rows = mat_1.length()
     col = mat_2[0].length()
@@ -13,8 +8,3 @@ def classical(mat_1, mat_2):
             for k in (range (0, (mat_f.range()-1))):
                 mat_f[i][j] += mat_1[i][k] * mat_2[k][j]
     print(mat_f)
-def div_n_conq(mat_1, mat_2):
-    if mat_1.length > 4:
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
