983cc2a82aef852d04bc4fb487e6908ca9dc0bdf
rainyday21 <rainydayscholar21@gmail.com>
2023-03-30 16:22:34 -0700

    basic classical finished

--- main.py
+++ main.py
@@ -6,9 +6,15 @@
 def classical(mat_1, mat_2):
     rows = mat_1.length()
     col = mat_2[0].length()
-    mat_f = []
+    mat_f = [[0]*rows]*col
     for i in (range (0,(mat_1.range()-1))):
         for j in (range (0, mat_2.range()-1)):
+            mat_f[i][j] = 0
+            for k in (range (0, (mat_f.range()-1))):
+                mat_f[i][j] += mat_1[i][k] * mat_2[k][j]
+    print(mat_f)
+def div_n_conq(mat_1, mat_2):
+    if mat_1.length > 4:
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
