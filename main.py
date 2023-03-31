37ef7dc2e650c446a88eee331f2d01c1b7a9b55d
russellric <116527694+russellric@users.noreply.github.com>
2023-03-30 22:25:27 -0700

    update main - russell
    fixing classical

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 def classical(mat_1, mat_2):
-    rows = mat_1.length()
-    col = mat_2[0].length()
-    mat_f = [[0]*rows]*col
-    for i in (range (0,(mat_1.range()-1))):
-        for j in (range (0, mat_2.range()-1)):
-            mat_f[i][j] = 0
-            for k in (range (0, (mat_f.range()-1))):
-                mat_f[i][j] += mat_1[i][k] * mat_2[k][j]
-    print(mat_f)
+    rows = len(mat_1)
+    col = len(mat_2[0])
+    mat_f = []
+    for i in (range(rows)):
+        row = []
+        for j in (range(col)):
+            sum = 0
+            for k in (range(len(mat_2))):
+                sum += mat_1[i][k] * mat_2[k][j]
+            row.append(sum)
+        mat_f.append(row)
+    print('classical output:',mat_f)
+
+def main():
+    matrix1 = [[1,2],[3,4]]
+    matrix2 = [[4,5,6],[7,8,9]]
+    classical(matrix1,matrix2)
+
+main()
