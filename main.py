1ccf7c58dbc3f0578f98c28ae0050bd0141544e3
rainyday21 <rainydayscholar21@gmail.com>
2023-03-30 21:54:31 -0700

    started div & conq

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def classical(mat_1, mat_2):
             for k in (range (0, (mat_f.range()-1))):
                 mat_f[i][j] += mat_1[i][k] * mat_2[k][j]
     print(mat_f)
+
+def div_n_conq(mat_1, mat_2, amt):
+    if (mat_1)
+    
+    if (len(mat_1) > 4 or len(mat_2) > 4):
+        for i in range(0, 3):
+            div_n_conq()
