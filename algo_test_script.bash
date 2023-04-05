81b4c6b3cff8341e4cdbf67ccf232f3b1ce13e2e
rainyday21 <rainydayscholar21@gmail.com>
2023-04-05 15:05:08 -0700

    updated script

--- algo_test_script.bash
+++ algo_test_script.bash
@@ -1,19 +1,13 @@
 #!/bin/bash
-
-echo "Testing algorithms: "
-
-javac classic_mm.java strassen_mm.java div_n_conq_mm.java
-read -p "Enter # of elements(blank for indiv prompt): " ELM
-if [[ $ELM -gt 0 ]]; then
-time java classic_mm $ELM
-sleep 2
-time java div_n_conq_mm $ELM
-sleep 2
-time java strassen_mm $ELM
+if [[ $1 -gt 0 ]]; then
+  y=$1
 else
-time java classic_mm
-sleep 2
-time java div_n_conq_mm
-sleep 2
-time java strassen_mm
-fi   
+  read -p "Enter amount of tests done" y
+fi
+  x=0
+  while [[ $x -le $y ]]; do
+    java composite_mm $x $x 3 >> results.txt
+    done
+
+
+
